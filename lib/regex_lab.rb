1359c6c1ae4d0f44d73cf53b53ce7ec5e2f515f1
climb719 <mary.oldweiler@gmail.com>
2021-01-22 12:18:35 -0500

    commit tree ec830f7ddc53e427191354fe29eea560e199de1e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/\b[AEIOU]|\b[aeiou]/)
+        return true
+    else
+        return false 
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    five_letter_array = text.split(" ")
+    five_letter_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   if text.match(/^[A-Z]/) && text.match(/[[:punct:]]$/) 
+    return true
+   else
+    return false
+   end
 end
 
 def valid_phone_number?(phone)
+    if phone.match(/([0-9] ?){10}/) || 
+    phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || 
+    phone.match(/\b([0-9]{7})\b/)
+    return true
+    else
+        return false 
+    end 
+end 
 
-end
